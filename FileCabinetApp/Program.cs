349c2a754913f966e802683dde579d972b098ad6
Kirill <kvusov@bk.ru>
2024-12-29 20:23:26 +0300

    Refactor user input validation.

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -14,6 +14,166 @@ namespace FileCabinetApp
         private const int DescriptionHelpIndex = 1;
         private const int ExplanationHelpIndex = 2;
         private static IFileCabinetService? fileCabinetService;
+        private static string typeOfTheRules = "default";
+
+        private static Func<string, Tuple<bool, string, string>> stringConverter = (string str) =>
+        {
+            return new Tuple<bool, string, string>(!string.IsNullOrEmpty(str), "String is null or empty", str);
+        };
+
+        private static Func<string, Tuple<bool, string, DateTime>> dateConverter = (string str) =>
+        {
+            DateTime dateOfBirth;
+            return new Tuple<bool, string, DateTime>(DateTime.TryParseExact(str, "MM/dd/yyyy", null, DateTimeStyles.None, out dateOfBirth), "String is null or empty", dateOfBirth);
+        };
+
+        private static Func<string, Tuple<bool, string, char>> charConverter = (string str) =>
+        {
+            char favouriteNumeral;
+            return new Tuple<bool, string, char>(char.TryParse(str, out favouriteNumeral), "This is not a char", favouriteNumeral);
+        };
+
+        private static Func<string, Tuple<bool, string, short>> shortConverter = (string str) =>
+        {
+            short age;
+            return new Tuple<bool, string, short>(short.TryParse(str, out age), "This is not a number", age);
+        };
+
+        private static Func<string, Tuple<bool, string, decimal>> decimalConverter = (string str) =>
+        {
+            decimal income;
+            return new Tuple<bool, string, decimal>(decimal.TryParse(str, out income), "This is not a number", income);
+        };
+
+        private static Func<DateTime, Tuple<bool, string>> dateValidator = (DateTime dateOfBirth) =>
+        {
+            if (typeOfTheRules.Equals("default", StringComparison.Ordinal))
+            {
+                if (dateOfBirth.CompareTo(DateTime.Now) >= 0 || dateOfBirth.CompareTo(new DateTime(01 / 01 / 1950)) <= 0)
+                {
+                    return new Tuple<bool, string>(false, "Your input date must be less then todays date and greater than 1 Jan of 1950");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+            else
+            {
+                if (dateOfBirth.CompareTo(DateTime.Now) >= 0 || dateOfBirth.CompareTo(new DateTime(01 / 01 / 1950)) <= 0)
+                {
+                    return new Tuple<bool, string>(false, "Your input date must be less then todays date and greater than 1 Jan of 1950");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+        };
+
+        private static Func<string, Tuple<bool, string>> stringValidator = (string str) =>
+        {
+            if (typeOfTheRules.Equals("default", StringComparison.Ordinal))
+            {
+                if (string.IsNullOrWhiteSpace(str) || str.Length < 2 || str.Length > 60)
+                {
+                    return new Tuple<bool, string>(false, "Your input length must be more than 2 and less than 60 and not all the spaces");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(str) || str.Length < 2 || str.Length > 60)
+                {
+                    return new Tuple<bool, string>(false, "Your input length must be more than 2 and less than 60 and not all the spaces");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+        };
+
+        private static Func<char, Tuple<bool, string>> charValidator = (char favouriteNumeral) =>
+        {
+            if (typeOfTheRules.Equals("default", StringComparison.Ordinal))
+            {
+                if (favouriteNumeral > '9' || favouriteNumeral < '0')
+                {
+                    return new Tuple<bool, string>(false, "Your input numeral must be between 0 and 9");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+            else
+            {
+                if (favouriteNumeral > '5' || favouriteNumeral < '0')
+                {
+                    return new Tuple<bool, string>(false, "Your input numeral must be between 0 and 5");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+        };
+
+        private static Func<short, Tuple<bool, string>> shortValidator = (short age) =>
+        {
+            if (typeOfTheRules.Equals("default", StringComparison.Ordinal))
+            {
+                if (age > 100 || age < 0)
+                {
+                    return new Tuple<bool, string>(false, "Your input age must be positive and less than 100");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+            else
+            {
+                if (age > 20 || age < 0)
+                {
+                    return new Tuple<bool, string>(false, "Your input age must be positive and less than 20");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+        };
+
+        private static Func<decimal, Tuple<bool, string>> decimalValidator = (decimal income) =>
+        {
+            if (typeOfTheRules.Equals("default", StringComparison.Ordinal))
+            {
+                if (income > 2000000 || income < 350)
+                {
+                    return new Tuple<bool, string>(false, "Your input income must be more than 350 and less than 2000000");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+            else
+            {
+                if (income > 500 || income < 0)
+                {
+                    return new Tuple<bool, string>(false, "Your input income must be positive and less than 500");
+                }
+                else
+                {
+                    return new Tuple<bool, string>(true, "Everything is fine");
+                }
+            }
+        };
 
         private static bool isRunning = true;
 
@@ -45,8 +205,6 @@ namespace FileCabinetApp
         /// <param name="args">Just arguments.</param>
         public static void Main(string[] args)
         {
-            string typeOfTheRules = "default";
-
             if (args != null)
             {
                 if (args.Length > 0)
@@ -160,26 +318,22 @@ namespace FileCabinetApp
             do
             {
                 Console.Write("First name: ");
-                string? firstname = Console.ReadLine();
+                string? firstname = ReadInput(stringConverter, stringValidator);
 
                 Console.Write("last name: ");
-                string? lastname = Console.ReadLine();
+                string? lastname = ReadInput(stringConverter, stringValidator);
 
-                DateTime dateOfBirth;
                 Console.Write("Date of birth: ");
-                DateTime.TryParseExact(Console.ReadLine(), "MM/dd/yyyy", null, System.Globalization.DateTimeStyles.None, out dateOfBirth);
+                DateTime dateOfBirth = ReadInput(dateConverter, dateValidator);
 
-                short age;
                 Console.Write("Age: ");
-                short.TryParse(Console.ReadLine(), null, out age);
+                short age = ReadInput(shortConverter, shortValidator);
 
-                char favouriteNumeral;
                 Console.Write("Favourite numeral: ");
-                _ = char.TryParse(Console.ReadLine(), out favouriteNumeral);
+                char favouriteNumeral = ReadInput(charConverter, charValidator);
 
-                decimal income;
                 Console.Write("Income: ");
-                _ = decimal.TryParse(Console.ReadLine(), out income);
+                decimal income = ReadInput(decimalConverter, decimalValidator);
 
                 ParameterObject obj = new ParameterObject()
                 {
@@ -235,26 +389,22 @@ namespace FileCabinetApp
                     if (numberOfTheRecord <= fileCabinetService.GetStat())
                     {
                         Console.Write("First name: ");
-                        string? firstname = Console.ReadLine();
+                        string? firstname = ReadInput(stringConverter, stringValidator);
 
-                        Console.Write("Last name: ");
-                        string? lastname = Console.ReadLine();
+                        Console.Write("last name: ");
+                        string? lastname = ReadInput(stringConverter, stringValidator);
 
-                        DateTime dateOfBirth;
                         Console.Write("Date of birth: ");
-                        DateTime.TryParseExact(Console.ReadLine(), "MM/dd/yyyy", null, System.Globalization.DateTimeStyles.None, out dateOfBirth);
+                        DateTime dateOfBirth = ReadInput(dateConverter, dateValidator);
 
-                        char favouriteNumeral;
-                        Console.Write("Favourite numeral: ");
-                        _ = char.TryParse(Console.ReadLine(), out favouriteNumeral);
-
-                        short age;
                         Console.Write("Age: ");
-                        short.TryParse(Console.ReadLine(), null, out age);
+                        short age = ReadInput(shortConverter, shortValidator);
 
-                        decimal income;
+                        Console.Write("Favourite numeral: ");
+                        char favouriteNumeral = ReadInput(charConverter, charValidator);
+
                         Console.Write("Income: ");
-                        _ = decimal.TryParse(Console.ReadLine(), out income);
+                        decimal income = ReadInput(decimalConverter, decimalValidator);
 
                         ParameterObject obj = new ParameterObject()
                         {
@@ -356,6 +506,40 @@ namespace FileCabinetApp
             {
                 Console.WriteLine(e.Message);
             }
+        }
+
+        private static T ReadInput<T>(Func<string, Tuple<bool, string, T>> converter, Func<T, Tuple<bool, string>> validator)
+        {
+            do
+            {
+                T value;
+
+                var input = Console.ReadLine();
+                if (input == null)
+                {
+                    throw new ArgumentException("The input is null");
+                }
+
+                var conversionResult = converter(input);
+
+                if (!conversionResult.Item1)
+                {
+                    Console.WriteLine($"Conversion failed: {conversionResult.Item2}. Please, correct your input.");
+                    continue;
+                }
+
+                value = conversionResult.Item3;
+
+                var validationResult = validator(value);
+                if (!validationResult.Item1)
+                {
+                    Console.WriteLine($"Validation failed: {validationResult.Item2}. Please, correct your input.");
+                    continue;
+                }
+
+                return value;
+            }
+            while (true);
         }
     }
 }
